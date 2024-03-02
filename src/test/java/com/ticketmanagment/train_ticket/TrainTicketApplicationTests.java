72915eb9a97198e748f0782ab8e9cd02ba1f3820
nancy-art <nancy.1822csi1023@kiet.edu>
2024-03-02 23:46:45 +0530

    created API

---
+++ src/test/java/com/ticketmanagment/train_ticket/TrainTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketmanagment.train_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
